37c810be734a0c8e6a9eee6a8a09b7d05bed3707
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-10-27 15:43:14 -0300

    chore: setup ESLint

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello João' });
-});
+app.get('/', (req, res) => res.json({ message: 'Hello João' }));
 
 app.listen(3333, () => {
   console.log('✔ Server started on port 3333!');

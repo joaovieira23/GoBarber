86ebf69bd78f516557233aa1e0b7bc70c06431e6
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-10-28 17:09:52 -0300

    feat: relationship models

--- src/database/migrations/1603903352159-CreateAppointments.ts
+++ src/database/migrations/1603903352159-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1603903352159 implements MigrationInterfa
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -17,14 +17,14 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (req, res) => {
   try {
-    const { provider, date } = req.body;
+    const { provider_id, date } = req.body;
     const parsedDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
     return res.json(appointment);
   } catch (err) {

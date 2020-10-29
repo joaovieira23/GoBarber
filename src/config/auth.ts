8ed9cbea96b27e7ba8803f22c7086353643a93fc
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-10-29 12:45:27 -0300

    feat: authenticated routes

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '87cc7acd948a3d32e8790dc3778845a8',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from "../config/auth";
 
 import User from '../models/Users';
 
@@ -33,9 +34,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, '87cc7acd948a3d32e8790dc3778845a8', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d'
+      expiresIn
     });
 
     return {

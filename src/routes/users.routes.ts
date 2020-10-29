3ca6994e2c4fb79b4341136fa8f203aa9bfbb7dc
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-10-29 15:54:40 -0300

    feat: file upload

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserServices';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -21,5 +26,15 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    console.log(req.file);
+    return res.json({ ok: true });
+  },
+);
 
 export default usersRouter;
